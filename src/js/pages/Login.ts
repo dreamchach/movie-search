222facd5f811679e87c1043d314c29c9b27df0be
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-10-14 11:24:19 +0900

    vanilla-TS

--- src/js/pages/Login.ts
+++ src/js/pages/Login.ts
@@ -19,7 +19,7 @@ const getHtml = () => {
             <div id="login-content">
                 <div id="login-center">
                     <img id="login-logo-one" src="${LogoOne}" alt="logo-one" />
-                    <a id="login-signup-link">지금 가입</a>
+                    <a class="login-signup-link" id='login'>지금 가입</a>
                     <p id="login-description">
                         영화에 대한 프리미엄 액세스를 얻으십시오.
                         디즈니 플러스 가격은 다음 주부터 1000원 인상됩니다.
